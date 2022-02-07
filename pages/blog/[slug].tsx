e6ee894027c339954583553513453a4899c15522
xxxx-oooo <xxxx-oooo@tor>
2022-02-07 18:36:10 +0800

    update

--- pages/blog/[slug].tsx
+++ pages/blog/[slug].tsx
@@ -7,6 +7,9 @@ import ReactMarkdown from 'react-markdown'
 import gfm from 'remark-gfm'
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faClock} from "@fortawesome/free-regular-svg-icons"
+import {faTags} from "@fortawesome/free-solid-svg-icons";
+import {ITag} from "../../components/interface";
+import Tag from "../../components/_tag";
 
 
 export const getServerSideProps: GetServerSideProps = async ({query}) => {
@@ -38,22 +41,38 @@ export const getServerSideProps: GetServerSideProps = async ({query}) => {
 
 
 const Index = ({data}: InferGetServerSidePropsType<typeof getServerSideProps>): JSX.Element => {
-  const _data = data
+  const _data= data
   const digest = `${_data.digest.slice(0, 100)}...`
   const created_at = moment.unix(_data.created_at_ts)
-  // const tagNumber = Number(_data.tags.length)
+  const tagNumber = Number(_data.tags.length)
 
   return (
       <Layout title={_data.title} description={digest} canonical={`https://jiaxin.im/blog/${_data.slug}`}>
         <div className="container flex justify-center mx-auto">
 
-          <div className="bg-white shrink w-8/12 py-6 px-8 rounded-lg shadow-md">
-            <h1 className="mb-4 text-3xl font-bold">{_data.title}</h1>
+          <div className="bg-white shrink w-8/12 p-8 rounded-lg shadow-md">
 
-            <div className="py-4 text-sm text-slate-400">
-              <FontAwesomeIcon icon={faClock}/>
-              <span className="ml-1 font-light">{created_at.format("yyyy-MM-DD")}</span>
+            <h1 className="mb-8 text-4xl font-bold text-center">{_data.title}</h1>
+
+            <div className="pt-4 flex justify-between">
+
+              <p className="text-sm text-gray-600">
+                {tagNumber > 0 ? <FontAwesomeIcon icon={faTags} className="mr-2"/> : ""}
+                {
+                  _data.tags?.map((tag: ITag) => (
+                    <>
+                      <Tag {...tag}/>
+                    </>
+                  ))
+                }
+              </p>
+
+              <div className="text-sm text-slate-400">
+                <FontAwesomeIcon icon={faClock}/>
+                <span className="ml-1 font-light">{created_at.format("yyyy-MM-DD")}</span>
+              </div>
             </div>
+
             <hr className="my-4 text-slate-400"/>
 
             <ReactMarkdown className="prose prose-neutral font-light max-w-none" remarkPlugins={[gfm]}>
