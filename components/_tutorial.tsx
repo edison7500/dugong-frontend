7c76ca6d382ec9dbe82cd4d425a582ba70a0e9b9
err <edison7500@gmail.com>
2022-02-10 18:24:37 +0800

    update

--- components/_tutorial.tsx
+++ components/_tutorial.tsx
@@ -3,10 +3,7 @@ import Image from 'next/image';
 import Link from "next/link";
 
 const Tutorial = (tutorial: Tutorial): JSX.Element => {
-
   const _cover_url = tutorial.cover_url ? tutorial.cover_url : ""
-  console.log(_cover_url)
-
   return (
     <>
       <div className="h-full border-1 shadow-md rounded-lg overflow-hidden">
