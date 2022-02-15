f3214a5b2a3b7b8a7df5ca7457a1a07578ce65af
err <edison7500@gmail.com>
2022-02-15 13:00:19 +0800

    cleanup useless code

--- pages/search.tsx
+++ pages/search.tsx
@@ -74,7 +74,6 @@ const Search: NextPage = (data: InferGetServerSidePropsType<typeof getServerSide
                      onInput={(e) => {
                        // @ts-ignore
                        setQuery(e.target.value)
-                       // console.log(e.target)
                      }}
               />
             </div>
@@ -95,16 +94,3 @@ const Search: NextPage = (data: InferGetServerSidePropsType<typeof getServerSide
 }
 
 export default Search;
-
-
-// <label htmlFor="website-admin"
-//        className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Username</label>
-// <div className="flex">
-// <span
-//   className="inline-flex items-center px-3 text-sm text-gray-900 bg-gray-200 rounded-l-md border border-r-0 border-gray-300 dark:bg-gray-600 dark:text-gray-400 dark:border-gray-600">
-// @
-// </span>
-//   <input type="text" id="website-admin"
-//          className="rounded-none rounded-r-lg bg-gray-50 border border-gray-300 text-gray-900 focus:ring-blue-500 focus:border-blue-500 block flex-1 min-w-0 w-full text-sm border-gray-300 p-2.5 "
-//          placeholder="Bonnie Green">
-// </div>
