5401d8ef31a5722c22cdb3b37a548f84695f045d
err <edison7500@gmail.com>
2022-08-08 11:36:34 +0800

    update footer

--- components/layout/footer.tsx
+++ components/layout/footer.tsx
@@ -9,7 +9,9 @@ const Index = () => {
             <div className="sm:w-2/3 text-center py-6">
               <p className="text-sm text-gray-700 font mb-2">
                 <Link href="https://nextjs.org">
-                  <a className="pr-2 hover:underline">Power by Next.js.</a>
+                  <a target="_blank" className="pr-2 hover:underline">
+                    Power by Next.js.
+                  </a>
                 </Link>
                 <span>The source code is licensed GNU. Build nicely.</span>
               </p>
